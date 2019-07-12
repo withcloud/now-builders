50d8de8336a6d5ef78eee117d0dd1abde0d13593
Steven <steven@ceriously.com>
2019-07-12 15:09:24 -0400

    [now-node] Use ncc before publishing

--- packages/now-node/build.sh
+++ packages/now-node/build.sh
@@ -34,3 +34,7 @@ rm -rf dist/helpers
 ncc build node_modules/source-map-support/register -o dist/source-map-support
 mv dist/source-map-support/index.js dist/source-map-support.js
 rm -rf dist/source-map-support
+
+ncc build src/index.ts -o dist/main
+mv dist/main/index.js dist/index.js
+rm -rf dist/main
